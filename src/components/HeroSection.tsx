d7aa4aab026471b25327be5b6034a6f91f7f2c73
Bagoudjare <97353000+Bagoudjare@users.noreply.github.com>
2025-08-13 17:05:27 +0000

    up head

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -54,9 +54,8 @@ export const HeroSection = () => {
             </button>
 
             <Link to="/partenaire">
-            <button
-              onClick={}
-              className="border-2 border-blue-400 text-blue-400 px-8 py-4 rounded-lg text-lg font-semibold hover:bg-blue-400 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
+            <button 
+            className="border-2 border-blue-400 text-blue-400 px-8 py-4 rounded-lg text-lg font-semibold hover:bg-blue-400 hover:text-white transition-all duration-300 transform hover:scale-105 shadow-lg"
             >
               Devenire partenaire de BT3.0
             </button>
