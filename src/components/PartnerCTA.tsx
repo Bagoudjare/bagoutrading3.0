c96d8a39b20688c3a82ec0e516ca8bd9e32757ba
Bagoudjare <97353000+Bagoudjare@users.noreply.github.com>
2025-09-13 20:52:04 +0000

    up

--- src/components/PartnerCTA.tsx
+++ src/components/PartnerCTA.tsx
@@ -14,7 +14,7 @@ export const PartnerCTA = () => {
             height: '120%',
             left: '-150%',
             top: '-10%',
-            animation: 'light-sweep-delayed 6s ease-in-out infinite',
+            animation: 'light-sweep-delayed 5s ease-in-out infinite',
             transform: 'skewX(-15deg)',
           }}
         />
